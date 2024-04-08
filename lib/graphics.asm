fd9342ea123df353f30e849d89088775e34143af
Svyatoslav <slavavarov30727@gmail.com>
2024-04-08 20:19:28 +0300

    Fix: revert all drawing code, no `SIGSEGV`.

--- lib/graphics.asm
+++ lib/graphics.asm
@@ -334,29 +334,6 @@ Graphics_new:
     push eax
     call ScreenImage_new
 
-    ; return.frame_bitmap_info = mem::zeroed()
-    MEM_ZEROED BITMAPINFO, edi+Graphics.frame_bitmap_info
-
-    ; return.frame_bitmap_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER)
-    mov dword [edi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biSize], BITMAPINFOHEADER.sizeof
-
-    ; return.frame_bitmap_info.bmiHeader.biPlanes = 1
-    mov dword [edi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biPlanes], 1
-
-    ; return.frame_bitmap_info.bmiHeader.biBitCount = 32
-    mov dword [edi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biBitCount], 32
-
-    ; return.frame_bitmap_info.bmiHeader.biCompression = BI_RGB
-    mov dword [edi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biCompression], BI_RGB
-
-    ; return.frame_device_context = CreateCompatibleDC(0)
-    push 0
-    call CreateCompatibleDC
-    mov dword [edi+Graphics.frame_device_context], eax
-
-    ; return.frame_bitmap = null
-    mov dword [edi+Graphics.frame_bitmap], 0
-
     ; Self::GLOBAL_PTR = edi
     mov dword [Graphics_GLOBAL_PTR], edi
 
@@ -393,10 +370,6 @@ Graphics_drop:
     call ScreenImage_drop
 
     DEBUGLN `Graphics::drop(<Self at `, esi, `>)`
-
-    ; DeleteDC(self.frame_device_context)
-    push dword [esi+Graphics.frame_device_context]
-    call DeleteDC
 
     ; Self::GLOBAL_PTR = null
     mov dword [Graphics_GLOBAL_PTR], 0
@@ -458,16 +431,10 @@ Graphics_on_redraw:
     push ebx
     mov ebp, esp
 
-    .paint_desc     equ -PAINTSTRUCT.sizeof - 4
-    .device_context equ -4
-
     .argbase        equ 16
     .window         equ .argbase+0
 
     .args_size      equ .window-.argbase+4
-    .stack_size     equ -.paint_desc
-
-    sub esp, .stack_size
 
     ; window := esi
     mov esi, dword [ebp+.window]
@@ -479,72 +446,9 @@ Graphics_on_redraw:
     test ebx, ebx
     jz .exit
 
-    ; device_context = BeginPaint(window.hwnd, &mut paint_desc)
-    lea eax, dword [ebp+.paint_desc]
-    push eax
-    push dword [esi+Window.hwnd]
-    call BeginPaint
-    mov dword [ebp+.device_context], eax
+    
 
-    ; graphics.image.fill(COLOR_RGB(0, 0, 0))
-    push COLOR_RGB(38, 26, 27)
-    lea eax, dword [ebx+Graphics.image]
-    push eax
-    call ScreenImage_fill
-
-    ; BitBlt(device_context,
-    ;        paint_desc.rcPaint.left, paint_desc.rcPaint.top,
-    ;        paint_desc.rcPaint.right - paint_desc.rcPaint.left, paint_desc.rcPaint.bottom - paint_desc.rcPaint.top,
-    ;        graphics.frame_device_context,
-    ;        paint_desc.rcPaint.left, paint_desc.rcPaint.top,
-    ;        SRCCOPY)
-    push SRCCOPY
-    push dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.top]
-    push dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.left]
-    push dword [ebx+Graphics.frame_device_context]
-    mov eax, dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.bottom]
-    sub eax, dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.top]
-    push eax
-    mov eax, dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.right]
-    sub eax, dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.left]
-    push eax
-    push dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.top]
-    push dword [ebp+.paint_desc+PAINTSTRUCT.rcPaint+RECT.left]
-    push dword [ebp+.device_context]
-    call BitBlt
-
-    ; SelectObject(graphics.frame_device_context, graphics.frame_bitmap)
-    push dword [ebx+Graphics.frame_bitmap]
-    push dword [ebx+Graphics.frame_device_context]
-    call SelectObject
-
-    ; ; BitBlt(device_context, 0, 0, window.width, window.height, graphics.frame_device_context, 0, 0, SRCCOPY)
-    ; push SRCCOPY
-    ; push 0
-    ; push 0
-    ; push dword [ebx+Graphics.frame_device_context]
-    ; push dword [esi+Window.height]
-    ; push dword [esi+Window.width]
-    ; push 0
-    ; push 0
-    ; push dword [ebp+.device_context]
-    ; call BitBlt
-
-    ; EndPaint(window.hwnd, &paint_desc)
-    lea eax, dword [ebp+.paint_desc]
-    push eax
-    push dword [esi+Window.hwnd]
-    call EndPaint
-
-    ; ; ReleaseDC(window.hwnd, device_context)
-    ; push dword [ebp+.device_context]
-    ; push dword [esi+Window.hwnd]
-    ; call ReleaseDC
-
-.exit:
-    add esp, .stack_size
-
-    pop ebx
+.exit:    pop ebx
     pop esi
     pop ebp
     ret .args_size
@@ -601,60 +505,7 @@ Graphics_init_image:
 
     DEBUGLN `Graphics::init_image(`, dword [ebp+.width], `, `, dword [ebp+.height], `)`
 
-    ; self.image.resize(width, height)
-    push dword [ebp+.height]
-    push dword [ebp+.width]
-    lea eax, dword [esi+Graphics.image]
-    push eax
-    call ScreenImage_resize
     
-    ; self.frame_bitmap_info.bmiHeader.biWidth = width
-    mov eax, dword [ebp+.width]
-    mov dword [esi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biWidth], eax
-
-    ; self.frame_bitmap_info.bmiHeader.biHeight = height
-    mov eax, dword [ebp+.height]
-    mov dword [esi+Graphics.frame_bitmap_info+BITMAPINFO.bmiHeader+BITMAPINFOHEADER.biHeight], eax
-
-    ; if !self.frame_bitmap.is_null() {
-    cmp dword [esi+Graphics.frame_bitmap], 0
-    je .self_frame_bitmap_is_null
-
-        ; DeleteObject(self.frame_bitmap)
-        push dword [esi+Graphics.frame_bitmap]
-        call DeleteObject
-    
-    ; }
-    .self_frame_bitmap_is_null:
-
-    ; ; self.frame_bitmap = CreateDIBSection(null, &self.frame_bitmap_info, DIB_RGB_COLORS, &mut self.image.data_ptr, 0, 0)
-    ; push 0
-    ; push 0
-    ; lea eax, dword [esi+Graphics.image+ScreenImage.data_ptr]
-    ; push eax
-    ; push DIB_RGB_COLORS
-    ; lea eax, dword [esi+Graphics.frame_bitmap_info]
-    ; push eax
-    ; push 0
-    ; call CreateDIBSection
-    ; mov dword [esi+Graphics.frame_bitmap], eax
-
-    ; self.frame_bitmap = CreateBitmap(width, height, 1, 32, self.image.data_ptr)
-    push dword [esi+Graphics.image+ScreenImage.data_ptr]
-    push 32
-    push 1
-    push dword [ebp+.height]
-    push dword [ebp+.width]
-    call CreateBitmap
-    mov dword [esi+Graphics.frame_bitmap], eax
-
-    DEBUGLN `frame_device_context = `, dword [esi+Graphics.frame_device_context]
-    DEBUGLN `frame_bitmap = `, dword [esi+Graphics.frame_bitmap]
-
-    ; SelectObject(self.frame_device_context, self.frame_bitmap)
-    push dword [esi+Graphics.frame_bitmap]
-    push dword [esi+Graphics.frame_device_context]
-    call SelectObject
 
     pop esi
     pop ebp
